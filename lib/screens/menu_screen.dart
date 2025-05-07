c468563be773efd0e2473cc993a6e09e193c56c3
Aidarde <nurbekaidar777@gmail.com>
2025-05-07 13:50:11 +0600

    baseline before patch

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -2,7 +2,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
 import '../providers/cart_provider.dart';
 import '../l10n/l10n_ext.dart';
 import '../theme/app_styles.dart';
